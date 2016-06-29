c1d4ab4370ffc19ce9c1a5409291c5ab7bb15d6e
inisire <inisire@gmail.com>
2016-06-29 20:35:44 +0300

    Errors

--- Json/JsonResponseBuilder.php
+++ Json/JsonResponseBuilder.php
@@ -5,7 +5,10 @@ namespace PrivateDev\Utils\Json;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Collection;
 use League\Fractal\Resource\Item;
-use PrivateDev\Utils\Form\FormErrorDecorator;
+use PrivateDev\Utils\Error\ErrorInterface;
+use PrivateDev\Utils\Error\ErrorList;
+use PrivateDev\Utils\Error\ErrorListInterface;
+use PrivateDev\Utils\Form\FormErrorAdapter;
 use Symfony\Component\Form\FormErrorIterator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -28,11 +31,7 @@ class JsonResponseBuilder
      */
     public function __construct(Manager $fractal)
     {
-        $this->body = [
-            'data' => [],
-            'error' => []
-        ];
-
+        $this->body = [];
         $this->fractal = $fractal;
     }
 
@@ -65,19 +64,51 @@ class JsonResponseBuilder
     }
 
     /**
-     * @param FormErrorIterator $error
+     * @param ErrorInterface $error
      *
      * @return $this
      */
-    public function setError(FormErrorIterator $error)
+    public function addError(ErrorInterface $error)
     {
-        $decorator = new FormErrorDecorator($error);
-
-        $this->setData('error', $decorator->format());
+        $this->body['errors'][] = $this->formatError($error);
 
         return $this;
     }
 
+    /**
+     * @param ErrorListInterface $errorList
+     *
+     * @return $this
+     */
+    public function addErrorList(ErrorListInterface $errorList)
+    {
+        foreach ($errorList->all() as $error) {
+            $this->addError($error);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param ErrorInterface $error
+     *
+     * @return array
+     */
+    protected function formatError(ErrorInterface $error)
+    {
+        if ($error->getCode() > 0) {
+            $formatted['code'] = $error->getCode();
+        }
+
+        if ($error->getOrigin() != "") {
+            $formatted['origin'] = $error->getOrigin();
+        }
+
+        $formatted['message'] = $error->getMessage();
+
+        return $formatted;
+    }
+    
     /**
      * @param int $statusCode
      *
