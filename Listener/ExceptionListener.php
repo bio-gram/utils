e87f417c1216aacbfc7fbfa82397ebf2d2a48c41
inisire <inisire@gmail.com>
2016-08-11 19:22:45 +0300

    Improved exception listener

--- Listener/ExceptionListener.php
+++ Listener/ExceptionListener.php
@@ -2,13 +2,19 @@
 
 namespace PrivateDev\Utils\Listener;
 
+use PrivateDev\Utils\Error\Error;
+use PrivateDev\Utils\Exception\ClientException;
+use PrivateDev\Utils\Json\JsonResponseBuilder;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener
 {
+    /**
+     * @var string
+     */
     private $env;
 
     /**
@@ -16,10 +22,23 @@ class ExceptionListener
      */
     private $logger;
 
-    public function __construct(string $env, LoggerInterface $logger)
+    /**
+     * @var JsonResponseBuilder
+     */
+    private $responseBuilder;
+
+    /**
+     * ExceptionListener constructor.
+     *
+     * @param string              $env
+     * @param LoggerInterface     $logger
+     * @param JsonResponseBuilder $responseBuilder
+     */
+    public function __construct(string $env, LoggerInterface $logger, JsonResponseBuilder $responseBuilder)
     {
         $this->env = $env;
         $this->logger = $logger;
+        $this->responseBuilder = $responseBuilder;
     }
 
     /**
@@ -27,27 +46,31 @@ class ExceptionListener
      */
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        if ($this->env === "dev") {
-            return;
-        }
-
         $exception = $event->getException();
 
-        $this->logException($exception, sprintf('Uncaught PHP Exception %s: "%s" at %s line %s',
-            get_class($exception),
-            $exception->getMessage(),
-            $exception->getFile(),
-            $exception->getLine()
-        ));
-
-        $response = new JsonResponse();
-
         if ($exception instanceof HttpExceptionInterface) {
-            $response->setData(['error' => $exception->getMessage()]);
-            $response->setStatusCode($exception->getStatusCode());
+            $response = $this->responseBuilder
+                ->addError(new Error($exception->getMessage()))
+                ->build($exception->getStatusCode());
+        } elseif ($exception instanceof ClientException) {
+            $response = $this->responseBuilder
+                ->addError(new Error($exception->getMessage()))
+                ->build(Response::HTTP_BAD_REQUEST);
         } else {
-            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
-            $response->setData(['error' =>  $this->env == "prod" ? "unknown error" : $exception->getMessage() . " " . $exception->getTraceAsString()]);
+            $message = $this->env == "prod"
+                ? "Unknown error"
+                : $exception->getMessage() . " " . $exception->getTraceAsString();
+
+            $response = $this->responseBuilder
+                ->addError(new Error($message))
+                ->build(Response::HTTP_INTERNAL_SERVER_ERROR);
+
+            $this->logException($exception, sprintf('Uncaught PHP Exception %s: "%s" at %s line %s',
+                get_class($exception),
+                $exception->getMessage(),
+                $exception->getFile(),
+                $exception->getLine()
+            ));
         }
 
         $event->setResponse($response);
